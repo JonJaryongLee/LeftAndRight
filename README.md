3d74a64f68840a1afab0fb06e44204eba093782c
JonJaryongLee <director9303@gmail.com>
2018-08-25 16:26:12 +0900

    버튼 추가

--- README.md
+++ README.md
@@ -17,9 +17,12 @@ npm run build
 
 For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
-## 작업내용
+## 초기작업내용
 1. Vue.js Webpack Simple로 초기구성을 하였습니다.
 2. Favicon설정 완료
 3. 기본 폰트는 나눔바른고딕입니다.
 4. Bootstrap Vue 를 Import하였습니다.
 5. fontAwesome cdn 추가
+
+## 8월 25일
+하단부 버튼 추가. 버튼을 누르면 각 언론사 모바일 창이 뜨도록 설정 (일부 언론사는 pc에서 모바일 주소로 안 들어가짐)
--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,23 @@
 <template>
-	<div>Footer</div>
+	<div>
+	
+		
+	  		<b-button :size="'lg'" :variant="'danger'" href="http://m.chosun.com">조선일보</b-button>
+	  		<b-button :size="'lg'" :variant="'danger'" href="https://mnews.joins.com">중앙일보</b-button>
+	  		<b-button :size="'lg'" :variant="'danger'" href="http://m.donga.com">동아일보</b-button>
+	  	
+	  	
+
+	
+	  		<b-button :size="'lg'" :variant="'warning'" href="http://m.hani.co.kr">한겨레</b-button>
+	  		<b-button :size="'lg'" :variant="'warning'" href="http://m.khan.co.kr">경향신문</b-button>
+	  		<b-button :size="'lg'" :variant="'warning'" href="http://m.ohmynews.com">오마이뉴스</b-button>
+		
+	
+	</div>
+
+<!-- button-2.vue -->
+
 </template>
 
 <script type="text/javascript">
