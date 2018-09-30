c418273f684dcf03cbbb58ab712f5d1650512ac8
golony6449 <ab6449@gmail.com>
2018-09-30 16:02:56 +0900

    Roll back some commit

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -10,7 +10,6 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
-var BundleTracker = require('webpack-bundle-tracker');
 
 const env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -120,8 +119,7 @@ const webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ]),
-    new BundleTracker({filename: './webpack-stats.json'})
+    ])
   ]
 })
 
